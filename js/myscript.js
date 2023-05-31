7d444bf17a9c92f38266050a2e1c3f7b13296513
rc2pc2 <petriccarcc@gmail.com>
2023-05-31 10:21:35 +0200

    added thumbnails and carousel details

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,65 @@
-
-const imagesList = [ 'stray.jpg', 'lol.jpg', 'bf3.jpg', 'cod.jpg', 'diablo.jpg' ];
+const imagesList = [
+    {
+        image: 'img/bf3.jpg',
+        title: 'Battlefield 3',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/cod.jpg',
+        title: 'Call Of Duty',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/diablo.jpg',
+        title: 'Diablo',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image:'img/stray.jpg',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/lol.jpg',
+        title: "League of Legends",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
 const carouselElement = document.querySelector('div.carousel');
-console.log(carouselElement);
+const thumbnailslement = document.querySelector('div.thumbnails');
 
-let activeIndex = 4;
+let activeIndex = 3;
+let hasCarouselStarted = false;
 
-for ( let i = 0 ; i < imagesList.length ; i++ ){
+// ? cerco l'elemento su cui iterare => imagesList
+// ? cerco di capire quante variabili mi servono => imageElement
+imagesList.forEach(( imageElement, index ) => {
     carouselElement.innerHTML +=
     `<div class="carousel-item">
-        <img src="./img/${imagesList[i]}" alt="Carousel slide image">
+        <img src="${imageElement.image}" alt="Carousel slide image of ${imageElement.title}">
+        <div class="item-info">
+            <h4 class="item-title">
+                ${imageElement.title}
+            </h4>
+            <p class="item-description">
+                ${imageElement.text}
+            </p>
+        </div>
     </div>`;
-}
 
-document.querySelectorAll('div.carousel-item')[activeIndex].classList.add('active');
+
+    const thumbnailItemElement = document.createElement('div');
+    thumbnailItemElement.classList.add('thumbnail-item');
+    thumbnailItemElement.innerHTML = `<img src="${imageElement.image}" alt="Carousel thumnbail image of ${imageElement.title}"></img>`;
+
+    thumbnailItemElement.addEventListener('click',  () => {
+        goToSlide(index);
+        activeIndex = index;
+    })
+
+
+    thumbnailslement.appendChild(thumbnailItemElement);
+});
+
+goToSlide(activeIndex);
+hasCarouselStarted = true;
 
 const prevButton = document.querySelector('div.previous-button');
 prevButton.addEventListener('click', function(){
@@ -23,8 +69,8 @@ prevButton.addEventListener('click', function(){
         activeIndex = activeIndex - 1;
     }
 
-    document.querySelector('div.carousel-item.active').classList.remove('active');
-    document.querySelectorAll('div.carousel-item')[activeIndex].classList.add('active');
+    goToSlide(activeIndex);
+
 });
 
 const nextButton = document.querySelector('div.next-button');
@@ -35,6 +81,19 @@ nextButton.addEventListener('click', function(){
         activeIndex = activeIndex + 1;
     }
 
-    document.querySelector('div.carousel-item.active').classList.remove('active');
-    document.querySelectorAll('div.carousel-item')[activeIndex].classList.add('active');
+    goToSlide(activeIndex);
 });
+
+
+function goToSlide(slideIndex){
+
+    if ( hasCarouselStarted )
+        document.querySelector('div.carousel-item.active').classList.remove('active');
+
+    document.querySelectorAll('div.carousel-item')[slideIndex].classList.add('active');
+
+    if ( hasCarouselStarted )
+        document.querySelector('div.thumbnail-item.active').classList.remove('active');
+
+    document.querySelectorAll('div.thumbnail-item')[slideIndex].classList.add('active');
+}
